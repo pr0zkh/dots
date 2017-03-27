b82a46eb4ae4022746472a19cc6357d6d971bbc3
Yurii Banin <yurii_banin@epam.com>
2017-03-27 16:55:33 +0300

    Initial commit (basic Java REST app based on Spring Boot ver. 1.5.2)

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# dots
+# dots - game which kept me awake during the long and boring university lessons. For more details please refer to this article: https://en.wikipedia.org/wiki/Dots_(game)
+
+## Frameworks, utilities etc.
+
+### dots-core
+* jdk 1.8.x
+* gradle 3.4.1
+* spring-boot 1.5.2
+* spock 1.0 (groovy 2.4)
+* junit 4.12
+
+### dots-web
+* TBA
+
+## Build, run, maintain
+
+### dots-core
+From the root folder execute: **gradlew build** - sources will be compiled and packed to jar file located at *build/libs/* (*build/libs/dots-core-0.1.0.jar* by defualt).
+Jar mentioned above is executable jar, in order to run the app execute from the root folder: *java -jar build/libs/dots-core-0.1.0.jar*.
+Application will be started at localhost port 8080 (some spring-boot+jetty magic, we'll figure out how it works later).
+You can check whether app is running by accessing http://localhost:8080 (greeting message should be displayed by default).
+
+### dots-web
+TBA
---
+++ dots-core/src/main/java/com/dots/App.java
@@ -0,0 +1,34 @@
+package com.dots;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
+
+/**
+ * For more details see https://spring.io/guides/gs/spring-boot#_create_an_application_class
+ */
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+            System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+            String[] beanNames = ctx.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+            for (String beanName : beanNames) {
+                System.out.println(beanName);
+            }
+        };
+    }
+
+}
